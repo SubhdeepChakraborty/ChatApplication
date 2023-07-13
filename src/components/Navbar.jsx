e1baeb3168c0da35b18fb86769b6be17578fac37
subhdeepchakraborty <chakrabortysubhdeep@icloud.com>
2023-07-13 15:55:05 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,31 +3,36 @@ import React from "react";
 import { auth } from "../firebase";
 import { Link } from "react-router-dom";
 import { useGlobalHook } from "../context/AuthContext";
-
+import LogoutIcon from "@mui/icons-material/Logout";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import { useChatGlobalHook } from "../context/ChatContext";
 const Navbar = () => {
   const { currentUser } = useGlobalHook();
-  // console.log(currentUser.photoURL);
+  const { setOpen } = useChatGlobalHook();
+
   return (
-    <div className="flex items-center justify-between p-5  bg-[#2f2d52] h-[60px]">
-      <div>
-        <span className="font-[Sen] text-lg logo text-white">Fire Chat</span>
-      </div>
-      <div className="flex items-center p-2 justify-center Lbutton gap-2">
-        <div className="pic">
-          <img
+    <div className="flex items-center justify-between p-3 bg-[#2f2d52] ">
+      <div className="chat">Fire Chat</div>
+      <div className="flex items-center justify-center gap-4">
+        <div className="picImage">
+          <LazyLoadImage
             src={currentUser.photoURL}
             className="h-[45px] w-[45px] rounded-full object-cover"
+            effect="blur"
+            threshold={100}
           />
         </div>
-        <div className="">
-          <Link to="/login">
-            <button
-              onClick={() => signOut(auth)}
-              className="ml-1 bg-[#525fe1] p-1 rounded px-1 text-sm font-semibold text-white"
-            >
-              Logout
-            </button>
-          </Link>
+        <div className="icon text-white">
+          <FirstPageIcon
+            className="cursor-pointer"
+            onClick={() => setOpen(false)}
+          />
+          <LogoutIcon
+            className="cursor-pointer absolute bottom-3 right-3"
+            onClick={() => signOut(auth)}
+          />
         </div>
       </div>
     </div>
